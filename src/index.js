d61b34b9ddeba8273cda449a87066d1556620351
Miriam-VB <miriamvillaltabelda@gmail.com>
2025-07-11 18:59:25 +0200

    Added javascript

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-console.log("hello");
+//import Typewriter from 'typewriter-effect/dist/core';
+
+let poemFormElement = document.querySelector("#poem-form");
+
+
+function generatePoem(event){
+    event.preventDefault();
+    let poemContainer = document.querySelector(".poem-btn");
+    poemContainer.innerHTML = "This is a poem";
+    
+
+    new Typewriter("#poem-form", {
+    strings: ["Roses are red"],
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+    });
+}
+
+poemFormElement.addEventListener("submit", generatePoem);
