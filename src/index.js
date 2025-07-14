89bdad5e7a88e1f4c105dad65a276243a9a25c57
Miriam-VB <miriamvillaltabelda@gmail.com>
2025-07-14 19:25:31 +0200

    Revise JS functionality

--- src/index.js
+++ src/index.js
@@ -1,20 +1,28 @@
-//import Typewriter from 'typewriter-effect/dist/core';
-
 let poemFormElement = document.querySelector("#poem-form");
+let poemContainer = document.querySelector("#poem-response");
 
+function displayPoem(response) {
+    poemContainer.innerHTML = response.data.answer;
 
-function generatePoem(event){
-    event.preventDefault();
-    let poemContainer = document.querySelector(".poem-btn");
-    poemContainer.innerHTML = "This is a poem";
-    
-
-    new Typewriter("#poem-form", {
-    strings: ["Roses are red"],
+    /* new Typewriter("#poem-response", {
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
-    });
+    });*/
+}
+
+function generatePoem(event){
+    event.preventDefault();
+
+    let input = document.querySelector("#request-input");
+
+    let apiKey="562a514fd3f985ad310a1c7b9o64at0a";
+    let prompt = `Generate a French poem about ${input.value}`;
+    let context = "Please, generate a 4 line poem in HTML. You should follow user instructions. Remove ''' and html at the begginging and in the end.";
+    let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+    
+    axios.get(apiUrl).then(displayPoem);
 }
 
 poemFormElement.addEventListener("submit", generatePoem);
